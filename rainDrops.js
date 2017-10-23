836f1491bb665b7a110935024995296ab36d9c43
Douglasz <stevenzenne@gmail.com>
2017-10-23 16:58:41 +0100

    rename variable in raindrop

--- rainDrops.js
+++ rainDrops.js
@@ -1,22 +1,22 @@
-const rainDrops=(n)=>{
+const rainDrops=(number)=>{
     // initializing the array for the different outcomes
   let drops =[];
 //testing for strings and returning a message.
-  if(typeof n=='string'){
+  if(typeof number=='string'){
    return 'please enter a number' 
   }
 //using 'if' statements to test for the 3 conditions of the prime factors
-   if(n%3===0){
+   if(number%3===0){
     drops+='pling';
   }
- if(n%5===0){
+ if(number%5===0){
     drops+='plang'
   }
- if(n%7===0){
+ if(number%7===0){
     drops+='plong';
     //testing for any other number outside the 3 cases given as instructed
-  }else if(n%3!==0 && n%5!==0 && n%7!==0){
-    return n;
+  }else if(number%3!==0 && number%5!==0 && number%7!==0){
+    return number;
   }
   return drops;
 }
